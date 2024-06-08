19af434184861ad608d04a82f8f4e8574a9cfcf4
Juce <iamjuceuwu@gmail.com>
2024-06-08 17:27:37 +0100

    Fixed footer & updated packages

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,12 +3,10 @@
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <hr class="my-6 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-8" />
             <div class="sm:flex sm:items-center sm:justify-between">
-                <!-- <img src="#" class="h-8 mr-3" /> -->
                 <ul class="flex flex-wrap items-center text-sm font-medium text-zinc-500 mb-4 md:mb-0">
                     <li>
-
-                        <a href="https://b.catgirlsare.sexy/hVhKcS-5ZKdy.mp4" class="mr-4 hover:underline md:mr-6 ">
-                            <Icon name="solar:cat-bold" class=""/> OnlyFans
+                        <a href="https://b.catgirlsare.sexy/hVhKcS-5ZKdy.mp4" class="mr-4 hover:underline md:mr-6">
+                            <Icon name="solar:cat-bold" class="" /> OnlyFans
                         </a>
                     </li>
                     <li>
@@ -17,16 +15,16 @@
                         </a>
                     </li>
                 </ul>
+                <span class="block text-sm text-zinc-500 sm:text-center select-none dark:text-zinc-400 mt-4 md:mt-0">
+                    © 2024
+                    <a href="https://juce.gg/" class="hover:underline"> juce.gg </a>
+                    . All Rights Reserved.
+                </span>
                 <span class="self-center text-md font-semi-bold whitespace-nowrap dark:text-zinc-200">
-                        Built With <Icon name="tabler:brand-nuxt" /> <Icon name="bxl:tailwind-css" />
+                    Built With <Icon name="tabler:brand-nuxt" />
+                    <Icon name="bxl:tailwind-css" />
                 </span>
             </div>
-            <span class="block text-sm text-zinc-500 sm:text-center select-none dark:text-zinc-400 mt-4 md:mt-0">
-                © 2024 
-                <a href="https://juce.gg/" class="hover:underline">juce.gg</a>. 
-                All Rights Reserved.</span>
         </div>
     </footer>
 </template>
-<script setup lang="ts">
-</script>
