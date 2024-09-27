6f399589856b58af38597bb057e55a6ee9c77e87
Juce <iamjuceuwu@gmail.com>
2024-09-27 19:17:54 +0100

    updating packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        'nuxt-headlessui',
-        'nuxt-icon'
-    ]
+  modules: [
+      '@nuxtjs/tailwindcss',
+      'nuxt-headlessui',
+      'nuxt-icon'
+  ],
+
+  compatibilityDate: '2024-09-27'
 })
